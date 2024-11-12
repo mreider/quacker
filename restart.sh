e79e6f0bc5afac6ae07e2fa7e269ed2ac98599be
Matthew Reider <matthew.reider@dynatrace.com>
2024-11-12 15:49:55 +0100

    shell scripts

---
+++ restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Restart Gunicorn backend service
+sudo systemctl restart backend
+
+# Restart Nginx
+sudo systemctl restart nginx
+
+# Completion message
+echo "Backend and Nginx services have been restarted."
