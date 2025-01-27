36a82053a0023ab82c4373c314565a984a240436
Matthew Reider <mreider@gmail.com>
2025-01-27 16:24:00 +0100

    added json content type

--- github_login_handlers.go
+++ github_login_handlers.go
@@ -104,6 +104,7 @@ func exchangeGitHubCodeForToken(code string) (string, error) {
 		return "", err
 	}
 	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
